54794b8543d849716ae15e16e365b81ebe89e944
LaVenganzaDelLadron <darkglitch5417@gmail.com>
2026-07-03 02:58:29 +0800

    test main.py

--- main.py
+++ main.py
@@ -1,16 +1,95 @@
-# This is a sample Python script.
-
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+import asyncio
+import sys
 
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+def version():
+    return "1.0.0"
+
+def help():
+    print("""
+    Usage:
+    
+    Victim:
+        python main.py -l
+    
+    Attacker:
+        python main.py -ol
+        python main.py -c <client_id> <command>
+    
+    Options:
+        -h, --help            Show help message
+        -v, --version         Show version information
+    
+    Victim Options:
+        -l, --listen          Start the client and wait for connections
+    
+    Attacker Options:
+        -ol, --online-list    List online clients
+        -c, --connect         Connect to a client
+    
+    Arguments:
+        <client_id>           Target client ID
+        <command>             Command to execute
+    """)
 
 
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
+def listen_mode():
+    print("listen mode")
 
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+def online_list_mode():
+    print("online list mode")
+
+def connect_mode(target, command):
+    print("connect mode")
+
+
+def main():
+    if len(sys.argv) < 2:
+        help()
+        return
+
+    if sys.argv[1] == "-l":
+        if len(sys.argv) != 3:
+            help()
+            return
+
+        asyncio.run(listen_mode())
+
+    elif sys.argv[1] == "-ol":
+        if len(sys.argv) != 3:
+            help()
+            return
+
+        asyncio.run(online_list_mode())
+
+    elif sys.argv[1] == "-c":
+        if len(sys.argv) != 4:
+            help()
+            return
+
+        target = sys.argv[2]
+        command = sys.argv[3]
+
+        asyncio.run(connect_mode(target, command))
+        
+    else:
+        help()
+
+if __name__ == "__main__":
+    main()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
