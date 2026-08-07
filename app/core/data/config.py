593478875ab02b8af51db5bb2da0890eb29c0549
darkglitch <darkglitch5417@gmail.com>
2026-08-07 03:22:41 +0000

    upload to pypi

--- app/core/data/config.py
+++ app/core/data/config.py
@@ -3,6 +3,6 @@ import os
 
 from dotenv import load_dotenv
 
-#HOST = "https://malware-signal.vercel.app"
-HOST = "http://127.0.0.1:8000"
+HOST = "https://malware-signal.vercel.app"
+#HOST = "http://127.0.0.1:8000"
 ROOM = "D4RKGLI7CH"
